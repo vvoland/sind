df1cee9c1aa1fb49558fe16be7df3b9503d88fdd
Paweł Gronowski <me@woland.xyz>
2025-02-03 16:59:52 +0100

    Remove /init.sh script
    Signed-off-by: Paweł Gronowski <me@woland.xyz>

--- init.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-share="/home/ubuntu/.local/share"
-mkdir -p "$share"
-mkdir -p "$share/Steam/steamapps/compatdata/289070/pfx/drive_c/users/steamuser/Documents/My Games/Sid Meier's Civilization VI/Saves"
-mkdir -p "$share/aspyr-media/Sid Meier's Civilization VI/Saves"
